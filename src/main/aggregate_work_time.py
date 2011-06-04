f51b740cb36a9d365be22ddb59f5500aafceefd6
Keita Kita <maoutwo@gmail.com>
2011-06-04 13:14:53 +0900

    Fix the output format.
    Because the previous output do not match to the requirement.

--- src/main/aggregate_work_time.py
+++ src/main/aggregate_work_time.py
@@ -9,6 +9,18 @@ import sys
 
 import work_recorder
 
+# Name of column that represents month. Format is YYYY-MM.
+COLUMN_MONTH = u'month'
+# Name of column that represents hours.
+COLUMN_HOURS = u'hours'
+
+# Key of time that represents a month.
+TIME_KEY_MONTH = COLUMN_MONTH
+# Key of time that represents a project.
+TIME_KEY_PROJECT = work_recorder.COLUMN_PROJECT
+# Key of time that represents hours a project.
+TIME_KEY_HOURS = COLUMN_HOURS
+
 def aggregate_work_time(start_day, end_day, conn):
     u"""
     Aggregate work times a project.
@@ -21,26 +33,38 @@ def aggregate_work_time(start_day, end_day, conn):
         A list of dictionary. Key is a name of project. Value is its work time.
     """
     cursor = conn.execute(u"""
-        select {project},
-            sum(strftime('%s', {day} || ' ' || {end}) -
-                strftime('%s', {day} || ' ' || {start})) / 60 / 60
+        select strftime('%Y-%m', {day}) as {month},
+            {project},
+            round(sum(strftime('%s', {day} || ' ' || {end}) -
+                strftime('%s', {day} || ' ' || {start})) / 60.0 / 60.0, 2) as {hours}
         from {work_time}
         where {day} between :start and :end
-        group by {project}
-        """.format(project = work_recorder.COLUMN_PROJECT,
+        group by {month}, {project}
+        order by {month} asc, {hours} desc
+        """.format(month = COLUMN_MONTH,
+            project = work_recorder.COLUMN_PROJECT,
             day = work_recorder.COLUMN_DAY,
             end = work_recorder.COLUMN_END,
+            hours = COLUMN_HOURS,
             start = work_recorder.COLUMN_START,
             work_time = work_recorder.TABLE_WORK_TIME),
         {u'start': start_day, u'end': end_day})
-    times = {}
-    for a_row in cursor:
-        times[a_row[0]] = a_row[1]
-    return times
+    return [
+            {TIME_KEY_MONTH: a_row[0],
+                TIME_KEY_PROJECT: a_row[1],
+                TIME_KEY_HOURS: a_row[2]}
+            for a_row in cursor]
 
 def print_result(times):
-    for a_project, a_hours in times.iteritems():
-        print u'%s: %s hours' % (a_project, a_hours)
+    previous_month = None
+    for a_time in times:
+        month = a_time[TIME_KEY_MONTH]
+        if month != previous_month:
+            if previous_month != None:
+                print
+            print month
+            previous_month = month
+        print u'\t%s: %s hours' % (a_time[TIME_KEY_PROJECT], a_time[TIME_KEY_HOURS])
 
 def main():
     u"""
--- src/test/test_aggregate_work_time.py
+++ src/test/test_aggregate_work_time.py
@@ -11,16 +11,20 @@ import os.path
 import sqlite3
 import unittest
 
-
 class TestRecordingWorkTimes(unittest.TestCase):
     def setUp(self):
         self.conn = sqlite3.connect(u':memory:')
         create_work_record.create_database(self.conn)       
 
+    def assert_time(self, month, project, hours, time):
+        self.assertEquals(month, time[aggregate_work_time.TIME_KEY_MONTH])
+        self.assertEquals(project, time[aggregate_work_time.TIME_KEY_PROJECT])
+        self.assertEquals(hours, time[aggregate_work_time.TIME_KEY_HOURS])
+
     def test_aggregate_normal(self):
         record_work_time.record_work_times(
                 record_work_time.convert_work_times(
-                    u'projectC', u'20101127',
+                    u'projectB', u'20101027',
                     [u'900', u'1300', u'1400', u'1900']),
                 self.conn)
         record_work_time.record_work_times(
@@ -39,9 +43,11 @@ class TestRecordingWorkTimes(unittest.TestCase):
                     [u'900', u'1200', u'1300', u'1800']),
                 self.conn)
         times = aggregate_work_time.aggregate_work_time(
-                u'2010-11-28', u'2010-11-30', self.conn)
-        self.assertEquals(16, times[u'projectA'])
-        self.assertEquals(9, times[u'projectB'])
+                u'2010-10-01', u'2010-11-30', self.conn)
+        self.assertEquals(3, len(times))
+        self.assert_time(u'2010-10', u'projectB', 9, times[0])
+        self.assert_time(u'2010-11', u'projectA', 16, times[1])
+        self.assert_time(u'2010-11', u'projectB', 9, times[2])
 
     def tearDown(self):
         self.conn.close()
